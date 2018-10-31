466be1abf8bc6f7920608b58bb976af0f5f770bb
gabyivanova <gabriella_iv13@abv.bg>
2018-10-31 12:48:50 +0200

    Add files via upload

---
+++ Magic-Letter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Magic_Letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var omit = char.Parse(Console.ReadLine());
+            var result = new List<string>();
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        result.Add(i.ToString() + j.ToString() + k.ToString());
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", result.Where(x => !x.Contains(omit))));
+
+        }
+    }
+}
