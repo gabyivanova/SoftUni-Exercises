6c582a491a2bc0ea8bff1adde1ffa7f7d9e37018
gabyivanova <gabriella_iv13@abv.bg>
2018-10-31 13:43:39 +0200

    Add files via upload

---
+++ Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _18.Extract_sentences_by_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToLower();
+            var sentences = Console.ReadLine();
+            var pattern = $"[^.!?;]*(({word}\\W)|(\\W{word}\\W))[^.!?;]*";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = Regex.Matches(sentences, pattern);
+
+            var count = 0;
+            foreach (Match match in matches)
+            {
+                if (count == 0)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+                else
+                {
+                    var sentence = Convert.ToString(match);
+                    Console.WriteLine(sentence.Substring(1));
+                }
+
+                count++;
+            }
+
+
+        }
+    }
+}
