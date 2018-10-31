b9a18ca59f0d22ad2c3e9a729bee23cbe57513ff
gabyivanova <gabriella_iv13@abv.bg>
2018-10-31 12:58:14 +0200

    Add files via upload

---
+++ Fibonacci-Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.FibonacciNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fib(n));
+        }
+
+        static int Fib(int n)
+        {
+            int f0 = 1;
+            int f1 = 1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int fNext = f0 + f1;
+                f0 = f1;
+                f1 = fNext;
+            }
+
+            return f1;
+        }
+    }
+}
